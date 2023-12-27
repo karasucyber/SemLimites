6b4091cc8e75fad3694399f9c9854c0aeb7c3a52
karasu_corp <aquillae777@gmail.com>
2023-12-26 23:37:10 -0300

    up

--- limites/Componentes/Parte2/Parte2.tsx
+++ limites/Componentes/Parte2/Parte2.tsx
@@ -2,30 +2,44 @@ import React from "react";
 import styled from "styled-components";
 
 
+
 const Container = styled.div({
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "90%",
     padding: "20px",
     margin: "20px",
-    height: "100%",
-    width: "100%",
 })
 
 const Titulo = styled.div({
     display: "flex",
     justifyContent: "center",
     fontSize: "35px",   
+    padding: "20px",
+    margin: "20px",
 })
 
 const ContainerT = styled.div({
     display: "flex",
-    justifyContent: "center",
+    justifyContent: 'flex-start',
+    fontSize: "35px",   
     height: "100%",
-    width: "100%",
+    width: "50%",
+    padding: "20px",
+    margin: "20px",
 })
 
 const ContainerV = styled.div({
-
+    display: "flex",
+    justifyContent: 'flex-end',
+    height: "100%",
+    width: "100%",
+    background: "white",
+    padding: "20px",
+    margin: "20px",
 })
 
 const ContainerN = styled.div({
@@ -36,18 +50,22 @@ const ContainerN = styled.div({
 export const Parte2 = () => {
     return(
         <Container> 
-            <Titulo>Quem nós somos</Titulo>
+                <Titulo> Quem somos</Titulo>
 
             <ContainerT>
-                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-                a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </ContainerT>
-
-            <ContainerV> 
+                <p>aaaaaaaaaaaaaaaaaaaaaaaa </p>
+                <ContainerV> 
+                <h1> aaaaaaaa</h1>
 
             </ContainerV>
+               
+            </ContainerT>
+
+            <Titulo> Números </Titulo>
+
             <ContainerN> 
+            <h1> aaaaaaaa</h1>
+
 
             </ContainerN>
         </Container>
