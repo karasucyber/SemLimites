6f2b2eba61d7ca1cdd75f7d8a5e93089298dbf22
karasu_corp <aquillae777@gmail.com>
2024-03-24 11:04:49 -0300

    up

--- limites/Componentes/NavBAr/Bar.tsx
+++ limites/Componentes/NavBAr/Bar.tsx
@@ -3,8 +3,6 @@ import styled, { keyframes } from 'styled-components';
 import { useMediaQuery } from "@react-hook/media-query";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-
-
 const borderAnimation = keyframes`
   0% {
     border-bottom-color: #d7e627; /* Cor inicial */
@@ -20,26 +18,20 @@ const borderAnimation = keyframes`
 const Container = styled.div`
   display: flex;
   margin: 10px;
-  width: 80%;
-  height: 10%;
+  width: 50%;
   justify-content: flex-end;
-  background: rgba(250, 250, 250, 0.10); 
   border-radius: 50px;
   transition: border-bottom 0.3s ease;
   &:hover {
     animation: ${borderAnimation} 1.5s infinite; /* Adiciona a animação no hover */
-    border-bottom-color: darkgray;}
-  @media (max-width: 768px) {
-    background: none;
-
+    border-bottom-color: darkgray;
   }
 `;
 
 const ContainerBar = styled.div`
-margin: 10px;
-padding: 10px;
-  width: 20%;
-  height: 50%;
+  margin: 10px;
+  padding: 10px;
+  height: 50px;
   display: flex;
   justify-content: center;
   font-size: 20px;
@@ -59,68 +51,37 @@ const StyledLink = styled.a`
   text-decoration: none;
   cursor: pointer;
   color: black;
-
   &:hover {
-color:  #D5E528;    
+    color:  #D5E528;    
   }
 `;
-
-const ContainerBar1 = styled.div`
-  padding: 5px;
-  width: 80%;
-  height: 70%;
-  display: flex;
-  border-radius: 10px;
-
-`;
-
-
-const ContainerIMG = styled.div({
-  backgroundImage: `url('Design sem nome (3) (1).png')`,
-  backgroundSize: "cover", 
-  backgroundPosition: "center", 
-  backgroundRepeat: "no-repeat", 
-  width: "150px",
-  height: "50px",
-  '@media (max-width: 758px)': {
-    width: "40px",
-    height: "40px",
-  backgroundImage: `url('Design sem nome (3) (1).png')`,
-    backgroundSize: "cover", 
-    backgroundPosition: "center", 
-    backgroundRepeat: "no-repeat", 
- 
-
-    },
-});
 
 const DropdownContainer = styled.div`
   position: relative;
   display: inline-block;
-  width: 100%;
-  height: 50px;
 `;
 
 const DropdownContent = styled.div<{ isOpen: boolean }>`
-  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
-  flex-direction: ${({ isOpen }) => (isOpen ? 'row' : 'column')};
-  position: block-inline;
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  position: absolute;
   top: 100%;
   right: 0;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
-  width: 100%;
-  height: 100%;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 10);
+  z-index: 1000;
+  @media (max-width: 768px) {
+    width: 160px;
+    min-width: unset;
+  }
 `;
 
-const Controller = styled.div({
-  width: "100%",
-  height: "100%",
-})
+const Controller = styled.div<{ isOpen: boolean }>`
+@media (max-width: 768px) {
+  height: ${({ isOpen }) => (isOpen ? '300px' : '100px')};
+  
+}
+`;
 
 export const NavBar = () => {
-
   const isMobile = useMediaQuery('(max-width: 768px)');
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
@@ -130,33 +91,12 @@ export const NavBar = () => {
 
   return (
     <Container>
-      {isMobile ? (
-         <Controller>
-                <DropdownContainer>
-          <ContainerBar onClick={toggleDropdown}>
+   <Controller isOpen={dropdownOpen}>
+   <DropdownContainer>
+        <ContainerBar onClick={toggleDropdown}>
           <GiHamburgerMenu size={30} />
-          </ContainerBar>
-          <DropdownContent isOpen={dropdownOpen}>
-            <ContainerBar>
-              <StyledLink href="/">Inicio</StyledLink>
-            </ContainerBar>
-            <ContainerBar>
-              <StyledLink href="/Cursos">Curso</StyledLink>
-            </ContainerBar>
-            <ContainerBar>
-              <StyledLink href="/Bolsista">Bolsistas</StyledLink>
-            </ContainerBar>
-            <ContainerBar>
-              <StyledLink href="/Empresas">Empresas</StyledLink>
-            </ContainerBar>
-          </DropdownContent>
-        </DropdownContainer>
-         </Controller>
-      ) : (
-        <>
-          <ContainerBar1>
-            <ContainerIMG />
-          </ContainerBar1>
+        </ContainerBar>
+        <DropdownContent isOpen={dropdownOpen}>
           <ContainerBar>
             <StyledLink href="/">Inicio</StyledLink>
           </ContainerBar>
@@ -169,11 +109,11 @@ export const NavBar = () => {
           <ContainerBar>
             <StyledLink href="/Empresas">Empresas</StyledLink>
           </ContainerBar>
-        </>
-      )}
+        </DropdownContent>
+      </DropdownContainer>
+   </Controller>
     </Container>
   );
 };
 
 export default NavBar;
-
